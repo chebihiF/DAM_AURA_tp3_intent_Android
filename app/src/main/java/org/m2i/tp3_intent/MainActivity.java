191ac9d2f54e252106db77acbbd8dcf973f3019b
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-31 11:45:20 +0100

    add phone web and map intent

--- app/src/main/java/org/m2i/tp3_intent/MainActivity.java
+++ app/src/main/java/org/m2i/tp3_intent/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1;
+    Button btn1, btnWeb, btnPhone, btnMap, btnMail, btnAgenda;
+    EditText phone_txt ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,40 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
 
         btn1 = findViewById(R.id.btn1);
+        btnWeb = findViewById(R.id.btnWeb);
+        btnPhone = findViewById(R.id.btnPhone);
+        btnMap = findViewById(R.id.btnMap);
+        btnMail = findViewById(R.id.btnMail);
+        btnAgenda = findViewById(R.id.btnAgenda);
+        phone_txt = findViewById(R.id.phoneNumber);
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webPage = Uri.parse("https://www.android.com");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webPage);
+                startActivity(webIntent);
+            }
+        });
+
+        btnPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri number = Uri.parse("tel:"+phone_txt.getText().toString());
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                startActivity(callIntent);
+            }
+        });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri location = Uri.parse("geo:0,0?q=paris");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                startActivity(mapIntent);
+            }
+        });
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/org/m2i/tp3_intent/MainActivity2.java
+++ app/src/main/java/org/m2i/tp3_intent/MainActivity2.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
-
     Button btn;
 
     @Override
