997d40c697e3d5a3c45d6880ed289881e76ed059
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-31 11:59:34 +0100

    final version

--- app/src/main/java/org/m2i/tp3_intent/MainActivity.java
+++ app/src/main/java/org/m2i/tp3_intent/MainActivity.java
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,6 +60,37 @@ public class MainActivity extends AppCompatActivity {
                 Uri location = Uri.parse("geo:0,0?q=paris");
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
                 startActivity(mapIntent);
+            }
+        });
+
+        btnMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("text/plain");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"mohammed@gmail.com","victor@gmail.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT,"email subject");
+                emailIntent.putExtra(Intent.EXTRA_TEXT,"email message text");
+                //emailIntent.putExtra(Intent.EXTRA_STREAM,Uri.parse("content://path_to_email_attachment"));
+                startActivity(emailIntent);
+
+            }
+        });
+
+        btnAgenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent calendarIntent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+                Calendar beginTime = Calendar.getInstance();
+                beginTime.set(2021, 0, 23, 7, 30);
+                Calendar endTime = Calendar.getInstance();
+                endTime.set(2021, 0, 23, 10, 30);
+                calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+                calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+                calendarIntent.putExtra(CalendarContract.Events.TITLE, "Ninja class");
+                calendarIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Secret dojo");
+
+                startActivity(calendarIntent);
             }
         });
 
