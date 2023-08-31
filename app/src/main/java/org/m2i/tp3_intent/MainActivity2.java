19ed9b1464a1a88abf7571d9eba6db7622fc8bc7
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-31 11:04:25 +0100

    add intent back

--- app/src/main/java/org/m2i/tp3_intent/MainActivity2.java
+++ app/src/main/java/org/m2i/tp3_intent/MainActivity2.java
@@ -2,13 +2,32 @@ package org.m2i.tp3_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        btn = findViewById(R.id.back);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
